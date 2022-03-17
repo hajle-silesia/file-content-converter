f9e73c31c6ca93a638fbe73d5920b4ce8d2f5c0a
mtweeman <mtweeman@gmail.com>
2022-03-17 18:14:51 +0100

    Primer

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.9-slim
+RUN apt-get update && apt-get install -y \
+    curl
+WORKDIR app
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY ./app/src .
+ENTRYPOINT ["python", "main.py"]
---
+++ app/src/api.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from file_content_converter import FileContentMonitor
+
+
+api = FastAPI()
+file_content_monitor = FileContentMonitor("./file_content_monitor/recipe.txt")
+
+
+@api.get("/content")
+async def content():
+    return {"content": file_content_monitor.content,
+            }
