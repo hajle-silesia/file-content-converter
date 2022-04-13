419c668077be15ac571fe9c2ceb26b135852b829
mtweeman <mtweeman@gmail.com>
2022-04-13 19:42:22 +0200

    Implemented converted class

--- app/src/file_content_converter.py
+++ app/src/file_content_converter.py
@@ -1,21 +1,21 @@
 import threading
-import hashlib
 from time import sleep
-import os
+
+import requests
 
 
-class FileContentMonitor(threading.Thread):
-    def __init__(self, path):
+class FileContentConverter(threading.Thread):
+    def __init__(self, url):
         threading.Thread.__init__(self, daemon=True)
 
         self.__monitoring_interval_time = 5
-        self.__path = path
+        self.__url = url
 
-        self.__last_modification_time = 0
-        self.__checksum = None
-        self.__content = ""
+        self.__response = None
+        self.__raw_content = ""
+        self.__content = {}
 
-        self.start()
+        self. start()
 
     @property
     def content(self):
@@ -23,58 +23,41 @@ class FileContentMonitor(threading.Thread):
 
     def run(self):
         while True:
-            self.__monitor_file()
+            self.__monitor_raw_content()
 
-    def __monitor_file(self):
-        if os.path.exists(self.__path):
-            self.__check_file()
-            self.__wait_time_interval()
+    def __monitor_raw_content(self):
+        if self.__raw_content_changed():
+            self.__update_raw_content()
+            self.__convert_raw_content()
+
+        self.__wait_time_interval()
+
+    def __raw_content_changed(self):
+        self.__get_new_raw_content()
+
+        return True if self.__raw_content != self.__new_raw_content else False
+
+    def __get_new_raw_content(self):
+        self.__get_response()
+        self.__extract_new_raw_content()
+
+    def __get_response(self):
+        self.__response = requests.get(self.__url)
+
+    def __extract_new_raw_content(self):
+        if self.__response.json():
+            self.__new_raw_content = self.__response.json()['content']
         else:
-            self.__clear_content()
+            self.__new_raw_content = None
 
-    def __check_file(self):
-        self.__get_last_modification_time()
+    def __update_raw_content(self):
+        self.__raw_content = self.__new_raw_content
 
-        if self.__modification_time_changed():
-            self.__calculate_file_checksum()
-
-            if self.__checksum_changed():
-                self.__update_last_modification_time()
-                self.__update_checksum()
-                self.__update_content()
-
-    def __get_last_modification_time(self):
-        self.__new_last_modification_time = os.path.getmtime(self.__path)
-
-    def __modification_time_changed(self):
-        return True if self.__new_last_modification_time > self.__last_modification_time else False
-
-    def __calculate_file_checksum(self):
-        self.__read_file()
-        self.__get_checksum()
-
-    def __read_file(self):
-        with open(self.__path, encoding="utf-8") as file:
-            self.__new_file_content = file.read()
-
-    def __get_checksum(self):
-        self.__new_checksum = hashlib.sha256(self.__new_file_content.encode()).hexdigest()
-
-    def __checksum_changed(self):
-        return True if self.__new_checksum != self.__checksum else False
-
-    def __update_last_modification_time(self):
-        self.__last_modification_time = self.__new_last_modification_time
-
-    def __update_checksum(self):
-        self.__checksum = self.__new_checksum
-
-    def __update_content(self):
-        self.__content = self.__new_file_content
+    def __convert_raw_content(self):
+        if self.__raw_content:
+            self.__content = self.__raw_content
+        else:
+            self.__content = {}
 
     def __wait_time_interval(self):
         sleep(self.__monitoring_interval_time)
-
-    def __clear_content(self):
-        if self.__content != "":
-            self.__content = ""
--- app/tests/test_file_content_converter.py
+++ app/tests/test_file_content_converter.py
@@ -1,105 +1,133 @@
 import unittest
+from unittest import mock
 from time import sleep
-import os
 
-from app.src.file_content_converter import FileContentMonitor
+from app.src.file_content_converter import FileContentConverter
 
 
-class TestFileContentMonitor(unittest.TestCase):
+unavailable_content = {'url': "http://unavailable/content", 'content': None}
+unavailable_to_empty_content = {'url': "http://unavailable_to_empty/content", 'counter': 0}
+empty_content = {'url': "http://empty/content", 'content': {}}
+empty_to_nonempty_content = {'url': "http://empty_to_non-empty/content", 'counter': 0}
+nonempty_content = {'url': "http://non-empty/content", 'content': {'test_key': "test_value"}}
+nonempty_to_empty_content = {'url': "http://non-empty_to_empty/content", 'counter': 0}
+nonempty_to_unavailable_content = {'url': "http://non-empty_to_unavailable/content", 'counter': 0}
+
+
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == unavailable_content['url']:
+        return MockResponse({'content': unavailable_content['content']}, 404)
+    elif args[0] == unavailable_to_empty_content['url']:
+        if unavailable_to_empty_content['counter'] == 0:
+            unavailable_to_empty_content['counter'] += 1
+            return MockResponse({'content': unavailable_content['content']}, 404)
+        else:
+            return MockResponse({'content': empty_content['content']}, 204)
+    elif args[0] == empty_content['url']:
+        return MockResponse({'content': empty_content['content']}, 204)
+    elif args[0] == empty_to_nonempty_content['url']:
+        if empty_to_nonempty_content['counter'] == 0:
+            empty_to_nonempty_content['counter'] += 1
+            return MockResponse({'content': empty_content['content']}, 204)
+        else:
+            return MockResponse({'content': nonempty_content['content']}, 200)
+    elif args[0] == nonempty_content['url']:
+        return MockResponse({'content': nonempty_content['content']}, 200)
+    elif args[0] == nonempty_to_empty_content['url']:
+        if nonempty_to_empty_content['counter'] == 0:
+            nonempty_to_empty_content['counter'] += 1
+            return MockResponse({'content': nonempty_content['content']}, 200)
+        else:
+            return MockResponse({'content': empty_content['content']}, 204)
+    elif args[0] == nonempty_to_unavailable_content['url']:
+        if nonempty_to_unavailable_content['counter'] == 0:
+            nonempty_to_unavailable_content['counter'] += 1
+            return MockResponse({'content': nonempty_content['content']}, 200)
+        else:
+            return MockResponse({}, 404)
+    else:
+        return MockResponse(None, 404)
+
+
+@mock.patch('app.src.file_content_converter.requests.get', side_effect=mocked_requests_get)
+class TestFileContentConverter(unittest.TestCase):
     def setUp(self):
         self.set_test_arguments()
         self.set_tested_objects()
         self.set_test_expected_results()
 
-    def tearDown(self):
-        self.remove_file()
-
     def set_test_arguments(self):
-        self.path = "./test.txt"
-        self.content = ".123inside\nfile"
+        pass
 
     def set_tested_objects(self):
-        self.file_content_monitor = FileContentMonitor(self.path)
+        pass
 
     def set_test_expected_results(self):
         pass
 
-    def test_Should_GetNoContent_When_FileIsNotAvailable(self):
+    def test_Should_GetEmptyDict_When_RawContentIsNotAvailable(self, mock_get):
+        self.file_content_converter = FileContentConverter(unavailable_content['url'])
+
         wait_monitoring_interval_time_with_buffer()
 
-        self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual({}, self.file_content_converter.content)
 
-    def test_Should_GetNoContent_When_NotAvailableFileIsCreated(self):
-        wait_monitoring_interval_time_with_buffer()
-        self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
+    def test_Should_GetEmptyDict_When_NotAvailableRawContentBecomesAvailableAndIsEmptyDict(self, mock_get):
+        self.file_content_converter = FileContentConverter(unavailable_to_empty_content['url'])
 
-        self.assertEqual("", self.file_content_monitor.content)
-
-    def test_Should_GetNoContent_When_FileIsEmpty(self):
-        self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
-
-        self.assertEqual("", self.file_content_monitor.content)
-
-    def test_Should_GetContent_When_EmptyFileContentIsUpdated(self):
-        self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
-        self.append_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-
-        self.assertEqual(self.content, self.file_content_monitor.content)
-
-    def test_Should_GetContent_When_FileHasContent(self):
-        self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-
-        self.assertEqual(self.content, self.file_content_monitor.content)
-
-    def test_Should_GetNoContent_When_FileContentWasRemoved(self):
-        self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-        self.write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
-
-        self.assertEqual("", self.file_content_monitor.content)
-
-    def test_Should_GetNoContent_When_FileWasRemoved(self):
-        self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-        self.remove_file()
         wait_monitoring_interval_time_with_buffer()
 
-        self.assertEqual("", self.file_content_monitor.content)
+        self.assertEqual({}, self.file_content_converter.content)
 
-    def test_Should_GetTheSameContent_When_FileWasSavedWithoutChangingContent(self):
-        self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-        self.append_nothing_to_file_and_wait_monitoring_interval_time_with_buffer()
+    def test_Should_GetEmptyDict_When_RawContentIsEmptyDict(self, mock_get):
+        self.file_content_converter = FileContentConverter(empty_content['url'])
 
-        self.assertEqual(self.content, self.file_content_monitor.content)
-
-    def test_Should_GetUpdatedContent_When_FileContentWasAdded(self):
-        self.write_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-        self.append_content_to_file_and_wait_monitoring_interval_time_with_buffer()
-
-        self.assertEqual(2 * self.content, self.file_content_monitor.content)
-
-    def write_content_to_file_and_wait_monitoring_interval_time_with_buffer(self):
-        with open(self.path, "w", encoding="utf-8") as file:
-            file.write(self.content)
         wait_monitoring_interval_time_with_buffer()
 
-    def write_nothing_to_file_and_wait_monitoring_interval_time_with_buffer(self):
-        with open(self.path, "w", encoding="utf-8") as file:
-            file.write("")
+        self.assertEqual(empty_content['content'], self.file_content_converter.content)
+
+    def test_Should_GetDictContent_When_EmptyDictRawContentIsUpdated(self, mock_get):
+        self.file_content_converter = FileContentConverter(empty_to_nonempty_content['url'])
+
         wait_monitoring_interval_time_with_buffer()
 
-    def append_nothing_to_file_and_wait_monitoring_interval_time_with_buffer(self):
-        with open(self.path, "a", encoding="utf-8") as file:
-            file.write("")
+        self.assertEqual(nonempty_content['content'], self.file_content_converter.content)
+
+    def test_Should_GetDictContent_When_RawContentIsNotEmptyDict(self, mock_get):
+        self.file_content_converter = FileContentConverter(nonempty_content['url'])
+
         wait_monitoring_interval_time_with_buffer()
 
-    def append_content_to_file_and_wait_monitoring_interval_time_with_buffer(self):
-        with open(self.path, "a", encoding="utf-8") as file:
-            file.write(self.content)
+        self.assertEqual(nonempty_content['content'], self.file_content_converter.content)
+
+    def test_Should_GetEmptyDict_When_RawContentBecomesEmptyDict(self, mock_get):
+        self.file_content_converter = FileContentConverter(nonempty_to_empty_content['url'])
+
         wait_monitoring_interval_time_with_buffer()
 
-    def remove_file(self):
-        if os.path.exists(self.path):
-            os.remove(self.path)
+        self.assertEqual(empty_content['content'], self.file_content_converter.content)
+
+    def test_Should_GetEmptyDict_When_RawContentBecomesNotAvailable(self, mock_get):
+        self.file_content_converter = FileContentConverter(nonempty_to_unavailable_content['url'])
+
+        wait_monitoring_interval_time_with_buffer()
+
+        self.assertEqual({}, self.file_content_converter.content)
 
 
 def wait_monitoring_interval_time_with_buffer():
-    sleep(6)
+    monitoring_interval_time = 5
+    buffer = 1
+    sleep(monitoring_interval_time + buffer)
+
+
+if __name__ == "__main__":
+    unittest.main()
