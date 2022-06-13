18671836201a8012b51f46bc728bd0b4b48d8ca7
mtweeman <mtweeman@gmail.com>
2022-06-13 19:44:40 +0200

    Applied Observer pattern

--- src/api.py
+++ src/api.py
@@ -1,20 +1,31 @@
-import os
 import base64
 import json
+import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 from src.file_content_converter import FileContentConverter
 
-
 api = FastAPI()
-host = os.getenv('FILE_CONTENT_MONITOR_SERVICE_HOST')
-port = os.getenv('FILE_CONTENT_MONITOR_SERVICE_PORT')
-url = f"http://{host}:{port}/content"
+host = os.getenv('FRONTEND_SERVICE_HOST')
+port = os.getenv('FRONTEND_SERVICE_PORT')
+url = f"http://{host}:{port}/update"
 file_content_converter = FileContentConverter(url)
+
+
+@api.get("/api")
+async def content():
+    return {"/content",
+            }
 
 
 @api.get("/content")
 async def content():
     return {"content": base64.b64encode(json.dumps(file_content_converter.content).encode()),
             }
+
+
+@api.post("/update")
+async def update(request: Request):
+    request_body = base64.b64decode(await request.body()).decode()
+    file_content_converter.update(request_body)
